a618e500ff1bffe37a76bd32d36780b1347b2242
naru-19 <narururu19@gmail.com>
2025-01-04 22:34:30 +0900

    update

--- src/settings.py
+++ src/settings.py
@@ -1,6 +1,22 @@
+import os
 from pathlib import Path
 
+from pydantic_settings import BaseSettings
 
-class Settings:
-    dataset_dir = Path("/app/resources/")
-    enc_dec_checkpoint = dataset_dir /"model_weights/encdec/600k.pth"
+
+class Settings(BaseSettings):
+    dataset_dir: Path = Path(__file__).absolute().parent.parent / "resources"
+    enc_dec_checkpoint: Path = dataset_dir / "model_weights/encdec/600k.pth"
+
+
+def get_settings():
+    return Settings()
+
+
+def main():
+    assert Settings().dataset_dir.exists()
+    assert Settings().enc_dec_checkpoint.exists()
+
+
+if __name__ == "__main__":
+    main()
